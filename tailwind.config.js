5cf8f9a6de5fc047f3a2c5647d930ec11b22fac9
Steve Winter <steve.winter@diva-e.com>
2021-05-06 21:54:56 +0200

    Config: add font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,13 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ["Poppins", "Arial", "sans-serif"],
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
