bc77ae950c1d9654bcf443f28852b3dd6b1265cb
Steve Winter <steve.winter@diva-e.com>
2021-05-19 08:02:43 +0200

    Feat: add branding component

---
+++ src/components/branding.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Branding() {
+  return (
+    <h1>
+      <span className="text-gray-600 text-xl font-bold mr-3">Steve Winter</span>
+      <span className="block xl:inline text-gray-400 text-xs font-bold">
+        Full-Stack Web Developer
+      </span>
+    </h1>
+  );
+}
+
+export default Branding;
