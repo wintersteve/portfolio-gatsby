81f3bad488a4af68eea415ce46d5a252fb86581e
Steve Winter <steve.winter@diva-e.com>
2021-05-22 07:42:50 +0200

    Feat: improve mobile responsiveness

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,15 +1,18 @@
 import React from "react";
 
-function Tile({ className, height, images, label }) {
+function Tile({ className, imageStyle, images, label }) {
   return (
-    <button className={`bg-gray-100 p-16 w-full rounded relative ${className}`}>
+    <button className={`bg-gray-100 w-full rounded relative ${className}`}>
       <h3 className="absolute font-semibold text-sm text-gray-400 top-3 left-3">
         {label.toUpperCase()}
       </h3>
-      <div className="flex justify-center">
+      <div
+        className="flex flex-wrap items-center justify-center"
+        style={{ minHeight: "6rem" }}
+      >
         {images.map((image) => (
           <img
-            className={`${height ? height : "h-24"} mx-4`}
+            className={`${imageStyle} my-4 md:my-0`}
             src={image}
             key={image}
           />
