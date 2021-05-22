2894d05ecd261e7e74f7ff2ccbf715c4a8974b45
Steve Winter <steve.winter@diva-e.com>
2021-05-22 07:43:31 +0200

    Fix: add check for callback function

--- src/components/fragment-link.js
+++ src/components/fragment-link.js
@@ -1,16 +1,19 @@
 import React from "react";
 
-function FragmentLink({ className, id, children, offset = 140 }) {
+function FragmentLink({ className, callback, children, id, offset = 140 }) {
   const scroll = () => {
     const top = document.getElementById(id)?.offsetTop;
-    console.log(top);
 
     window.scrollTo({ top: top - offset, behavior: "smooth" });
+
+    if (typeof callback === "function") {
+      callback();
+    }
   };
 
   return (
     <button
-      className="ml-2 px-3 py-2 text-gray-500 font-semibold text-sm transition duration-200 ease-in-out hover:bg-gray-100 hover:text-gray-600 rounded-full"
+      className={`ml-2 px-3 py-2 text-gray-500 font-semibold text-sm transition duration-200 ease-in-out hover:bg-gray-100 hover:text-gray-600 rounded-full ${className}`}
       onClick={scroll}
     >
       {children}
