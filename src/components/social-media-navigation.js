d0fe52b00e5ad6d8f9bf990a89aac04cd0a8741a
Steve Winter <steve.winter@diva-e.com>
2021-05-26 21:06:38 +0200

    Styling: update social media footer design

--- src/components/social-media-navigation.js
+++ src/components/social-media-navigation.js
@@ -2,9 +2,9 @@ import React from "react";
 
 import icons from "../data/social-media.json";
 
-function SocialMediaNavigation({ theme = "white", iconStyle }) {
+function SocialMediaNavigation({ theme = "white", iconStyle, style }) {
   return (
-    <nav className="items-center flex">
+    <nav className={`items-center flex ${style}`}>
       {icons.map((icon) => (
         <img
           alt={icon.images.alt}
