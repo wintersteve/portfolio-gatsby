e041afcd98f2ef066841758ea20081e7637946f6
Steve Winter <steve.winter@diva-e.com>
2021-06-12 16:52:42 +0200

    Feat: provide toast globally

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,8 @@
+import * as React from "react";
+import { ToastProvider } from "./src/providers/toast-provider";
+
 import "./src/css/index.css";
+
+export const wrapRootElement = ({ element }) => {
+  return <ToastProvider>{element}</ToastProvider>;
+};
