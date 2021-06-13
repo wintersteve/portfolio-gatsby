ed22f3704e179a96c4f5724297eb4ba4f19e0184
Steve Winter <steve.winter@diva-e.com>
2021-06-13 19:21:38 +0200

    Feat: use positional prop

--- src/components/branding.js
+++ src/components/branding.js
@@ -4,10 +4,16 @@ import UnderlinedText from "./underlined-text";
 function Branding() {
   return (
     <h1>
-      <UnderlinedText textClass="text-gray-600 text-xl font-bold">
+      <UnderlinedText
+        position={{ bottom: ".2rem" }}
+        textClass="text-gray-600 text-xl font-bold"
+      >
         Steve Winter
       </UnderlinedText>
-      <UnderlinedText textClass="block text-gray-400 text-xs font-bold">
+      <UnderlinedText
+        position={{ bottom: "0" }}
+        textClass="block text-gray-400 text-xs font-bold"
+      >
         Full-Stack Web Developer
       </UnderlinedText>
     </h1>
