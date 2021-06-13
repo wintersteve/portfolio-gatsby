ac7929f02771349c196ae5c571df648f0475b37f
Steve Winter <steve.winter@diva-e.com>
2021-06-13 19:17:44 +0200

    Feat: refactor using underlined text component

--- src/components/branding.js
+++ src/components/branding.js
@@ -1,12 +1,15 @@
 import React from "react";
+import UnderlinedText from "./underlined-text";
 
 function Branding() {
   return (
     <h1>
-      <span className="text-gray-600 text-xl font-bold mr-3">Steve Winter</span>
-      <span className="block text-gray-400 text-xs font-bold">
+      <UnderlinedText textClass="text-gray-600 text-xl font-bold">
+        Steve Winter
+      </UnderlinedText>
+      <UnderlinedText textClass="block text-gray-400 text-xs font-bold">
         Full-Stack Web Developer
-      </span>
+      </UnderlinedText>
     </h1>
   );
 }
