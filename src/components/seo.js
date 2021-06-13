f8b44ba21958d4e5ab15fd12563b5c92f9df7e17
Steve Winter <steve.winter@diva-e.com>
2021-06-13 21:01:27 +0200

    Feat: implement seo component

---
+++ src/components/seo.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
+
+const SEO = () => {
+  const { site } = useStaticQuery(query);
+
+  const { title, description, og } = site.siteMetadata;
+
+  return (
+    <Helmet title={title}>
+      <meta name="description" content={description} />
+
+      <meta property="og:description" content={og.description} />
+      <meta property="og:url" content={og.url} />
+      <meta property="og:type" content={og.type} />
+      <meta property="og:title" content={og.title} />
+    </Helmet>
+  );
+};
+
+const query = graphql`
+  query SEO {
+    site {
+      siteMetadata {
+        title
+        description
+        og {
+          description
+          title
+          url
+          type
+        }
+      }
+    }
+  }
+`;
+
+export default SEO;
