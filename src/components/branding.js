dfc63a898d6f55d30e3b979b7db656a000f5a971
Steve Winter <steve.winter@diva-e.com>
2021-06-13 13:21:22 +0200

    Styling: update layout

--- src/components/branding.js
+++ src/components/branding.js
@@ -4,7 +4,7 @@ function Branding() {
   return (
     <h1>
       <span className="text-gray-600 text-xl font-bold mr-3">Steve Winter</span>
-      <span className="block xl:inline text-gray-400 text-xs font-bold">
+      <span className="block text-gray-400 text-xs font-bold">
         Full-Stack Web Developer
       </span>
     </h1>
