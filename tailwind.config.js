0cf9bf89ac61473683bcfb5c992e7bdd0ac0dd46
Steve Winter <steve.winter@diva-e.com>
2021-06-13 18:46:39 +0200

    Feat: configure theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      gray: colors.coolGray,
+      white: colors.white,
+      primary: "#1f2937",
+      secondary: {
+        100: "#EEFAF5",
+        900: "#577569",
+      },
+      danger: "#D62828",
+    },
     fontFamily: {
       sans: ["Poppins", "Arial", "sans-serif"],
     },
-    extend: {},
   },
   variants: {
     extend: {},
