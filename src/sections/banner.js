3bb82dcc536db369efe508ce087d81d427a50d6a
Steve Winter <steve.winter@diva-e.com>
2021-06-14 08:11:07 +0200

    Feat: implement alternate banner design

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -1,61 +1,67 @@
 import React from "react";
-import avatar from "../images/avatar.svg";
 import ScrollAnimation from "react-animate-on-scroll";
+import Tile from "../components/tile";
+
+import webDesignImage from "../images/services/app.svg";
+import marketingImage from "../images/services/social-media-marketing.svg";
+import angularImage from "../images/services/angular.svg";
+import typescriptImage from "../images/services/typescript.svg";
+import graphqlImage from "../images/services/graphql.svg";
+import nuxtImage from "../images/services/nuxt.svg";
 
 function Banner() {
   return (
-    <section className="bg-gradient-to-b from-white to-gray-100 flex flex-col items-center justify-center pt-8 pb-20 md:py-28 lg:pt-40 lg:pb-48 2xl:mx-6 relative">
-      <div>
-        <ScrollAnimation
-          animateIn="fade"
-          className="flex flex-col md:flex-row items-start ml-0 lg:ml-6"
-        >
-          <img
-            alt="avatar"
-            className="h-56 md:h-64 lg:h-72 mx-auto md:mx-0 mb-12 md:mb-0 md:mr-6 lg:mr-12"
-            src={avatar}
-          />
-          <div className="px-6 md:px-0 mb-6 md:mb-0 lg:mt-2">
-            <h2 className="font-semibold ml-1 mb-1 opacity-75 text-lg">
-              Hi, I am Steve Winter
-            </h2>
-            <h3 className="font-bold mb-6 text-5xl md:text-6xl lg:text-7xl">
-              <span className="block">Full-Stack</span>
-              <span className="opacity-75">Web Developer</span>
-            </h3>
-            <p className="max-w-md ml-1 md:ml-2 text-md">
-              I'm a Germany-based software engineer who excels at building
-              digital experiences. Currently, I'm an engineer at Upstatement
-              focused on building accessible, human-centered products.
-            </p>
-          </div>
-        </ScrollAnimation>
-      </div>
-
-      <svg
-        className="absolute block bottom-0 left-0 w-full z-10 h-16"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 24 150 28 "
-        preserveAspectRatio="none"
+    <section className=" 2xl:mx-auto max-w-screen-2xl mt-10 mb-12 relative">
+      <ScrollAnimation
+        animateIn="fade"
+        className="flex flex-col justify-center md:flex-row items-start"
       >
-        <defs>
-          <path
-            id="gentle-wave"
-            d="M-160 44c30 0 
-    58-18 88-18s
-    58 18 88 18 
-    58-18 88-18 
-    58 18 88 18
-    v44h-352z"
-          />
-        </defs>
-        <g className="moveForever:1">
-          <use href="#gentle-wave" x="50" y="9" fill="rgb(229, 231, 235)" />
-        </g>
-        <g className="moveForever:2">
-          <use href="#gentle-wave" x="50" y="6" fill="#fff" />
-        </g>
-      </svg>
+        <img
+          alt="avatar"
+          className="h-auto mx-auto md:mx-0 mb-12 md:mb-0 md:mr-6 lg:mr-12 rounded-lg w-1/2"
+          src="images/image.png"
+        />
+        <div className="px-6 md:px-0 mb-6 md:mb-0 lg:mt-2">
+          <h2 className="font-semibold ml-1 mb-1 opacity-75 text-lg">
+            Hi, I am Steve Winter
+          </h2>
+          <h3 className="bg-secondary-100 font-bold mb-6 text-5xl md:text-6xl lg:text-7xl">
+            <span className="block">Full-Stack</span>
+            <span className="opacity-75">Web Developer</span>
+          </h3>
+          <p className="max-w-md ml-1 md:ml-2 mb-6 text-md">
+            I'm a Germany-based software engineer who excels at building digital
+            experiences. Currently, I'm an engineer at Upstatement focused on
+            building accessible, human-centered products.
+          </p>
+          <div className="grid grid-cols-2 gap-3 md:gap-6 lg:mb-6">
+            <ScrollAnimation animateIn="fade" delay={150} offset={1000}>
+              <Tile
+                className="mb-4 p-6 md:p-12 lg:mb-0"
+                imageStyle="h-12 md:h-16 mx-2 md:mx-4"
+                images={[webDesignImage]}
+                label={"Web Design"}
+              />
+            </ScrollAnimation>
+            <ScrollAnimation animateIn="fade" delay={150} offset={1000}>
+              <Tile
+                className="mb-4 p-6 md:p-12 lg:mb-0"
+                imageStyle="h-12 md:h-16 mx-2 md:mx-4"
+                images={[marketingImage]}
+                label={"SEO & Analytics"}
+              />
+            </ScrollAnimation>
+          </div>
+          <ScrollAnimation animateIn="fade" delay={150} offset={1000}>
+            <Tile
+              className="p-6 px-0 md:py-16 md:px-12"
+              imageStyle="h-12 md:h-12 mx-2 md:mx-6"
+              images={[typescriptImage, angularImage, nuxtImage, graphqlImage]}
+              label={"Programming"}
+            />
+          </ScrollAnimation>
+        </div>
+      </ScrollAnimation>
     </section>
   );
 }
