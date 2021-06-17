00e8fa5bec343c6ade84e8a3a2531b82659df314
Steve Winter <steve.winter@diva-e.com>
2021-06-17 23:10:39 +0200

    Feat: implement active state

--- src/components/hamburger.js
+++ src/components/hamburger.js
@@ -1,14 +1,30 @@
 import React from "react";
 
-function Hamburger({ className, handleClick }) {
+function Hamburger({ className, handleClick, isActive }) {
   return (
     <button
       className={`flex flex-col items-end ${className}`}
       onClick={handleClick}
     >
-      <div className="bg-gray-600 h-1 mb-1 w-8"></div>
-      <div className="bg-gray-600 h-1 mb-1 w-7"></div>
-      <div className="bg-gray-600 h-1 mb-1 w-5"></div>
+      <div
+        className="bg-gray-600 h-1 mb-1 transform transition-all w-8"
+        style={
+          isActive ? { transform: "matrix(1, 1, -1, 1, 1, 9) scale(0.7)" } : {}
+        }
+      ></div>
+      <div
+        className={`h-1 mb-1 w-7 ${
+          isActive ? "bg-transparent" : "bg-gray-600"
+        }`}
+      ></div>
+      <div
+        className={`bg-gray-600 h-1 mb-1 transform transition-all ${
+          isActive ? "w-8 " : "w-5"
+        }`}
+        style={
+          isActive ? { transform: "matrix(1, -1, 1, 1, 1, -6) scale(0.7)" } : {}
+        }
+      ></div>
     </button>
   );
 }
