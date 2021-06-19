1b2ec1edb6dff416d00a74031c313d3b368ae8ce
Steve Winter <steve.winter@diva-e.com>
2021-06-19 15:47:56 +0200

    Styling: update style

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,17 +1,9 @@
 import React from "react";
 
-function Editor() {
+function Editor({ className }) {
   return (
     <>
-      <div
-        style={{
-          maxWidth: "50%",
-          position: "relative",
-          bottom: "3rem",
-          right: "8rem",
-        }}
-        className="hidden lg:block bg-primary relative md:px-8 lg:px-0 col-start-1 col-span-full lg:col-start-1 xl:col-start-3 xl:col-end-4 row-start-1 row-end-2 xl:row-start-2 xl:row-end-5 self-center pt-8 lg:pt-0 rounded-md"
-      >
+      <div className={`bg-primary rounded-md ${className}`}>
         <div className="mx-auto lg:max-w-2xl xl:max-w-none">
           <div className="relative overflow-hidden md:rounded-xl shadow-2xl flex CodeWindow_root__1fMBP Hero_codeWindow__1W0zH bg-light-blue-500 pb-6 md:pb-0">
             <div className="absolute inset-0 bg-black bg-opacity-75"></div>
@@ -42,7 +34,7 @@ function Editor() {
                           padding: "1rem",
                         }}
                       >
-                        1 2 3 4 5 6 7 8 9 10 11 12
+                        1 2 3 4 5 6 7 8 9 10 11 12 13
                       </div>
                       <code className="flex-auto relative block text-white pt-4 pb-4 px-4 overflow-auto opacity-60">
                         {`<!DOCTYPE html>
@@ -54,7 +46,8 @@ function Editor() {
   <title>Steve Winter | Full-Stack Developer</title>
 </head>
 <body>
-  I program awesome applications
+   Hey, I am Steve Winter and I build extra-ordinary digital
+   products. Take a look at my experiences on the left.
 </body>
 </html>`}
                       </code>
