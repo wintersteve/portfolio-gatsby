0cd8216d19b3c458f5f979461e4c5c266d5fc02d
Steve Winter <steve.winter@diva-e.com>
2021-06-20 11:31:33 +0200

    Styling: improve responsiveness

--- src/sections/about.js
+++ src/sections/about.js
@@ -4,7 +4,7 @@ import ScrollAnimation from "react-animate-on-scroll";
 function About() {
   return (
     <section
-      className="bg-secondary-100 2xl:mx-6 py-20 md:py-28 lg:py-52 px-10 md:px-28 lg:px-48 xl:px-72 2xl:px-96"
+      className="bg-secondary-100 2xl:mx-6 py-20 md:py-28 lg:py-52 px-6 md:px-28 lg:px-48 xl:px-72 2xl:px-96"
       id="about"
     >
       <ScrollAnimation animateIn="fade" duration={0.75}>
@@ -13,7 +13,7 @@ function About() {
             About me
           </h2>
         </div>
-        <p className="text-md">
+        <p className="md:text-md">
           Since beginning my journey as a software engineer approximately 4
           years ago, I've done some freelance remote work, worked full-time in
           small to medium-sized companies, and additionally, have worked on
