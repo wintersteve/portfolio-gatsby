d496c64ea04e5ab50bef750c1a60eb64e9f21b27
Steve Winter <steve.winter@diva-e.com>
2021-06-20 10:30:13 +0200

    Feat: use fit content instead of max to prevent overflowing text / extend container class

--- src/components/underlined-text.js
+++ src/components/underlined-text.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Border from "./border";
 
-function UnderlinedText({ children, position, textClass }) {
+function UnderlinedText({ className, children, position, textClass }) {
   return (
-    <div className="relative w-max">
+    <div className={`relative w-fit ${className}`}>
       <span className={`relative z-20 ${textClass}`}>{children}</span>
       <Border style={position} />
     </div>
