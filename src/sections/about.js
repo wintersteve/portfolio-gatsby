3c0e45d44ba4ad1be7817dc9888f9e5e3a9a40ae
Steve Winter <steve.winter@diva-e.com>
2021-06-20 11:26:12 +0200

    Feat: update text and styling

--- src/sections/about.js
+++ src/sections/about.js
@@ -4,7 +4,7 @@ import ScrollAnimation from "react-animate-on-scroll";
 function About() {
   return (
     <section
-      className="bg-secondary-100 2xl:mx-6 py-20 md:py-28 lg:py-60 px-10 md:px-28 lg:px-48 xl:px-72 2xl:px-96"
+      className="bg-secondary-100 2xl:mx-6 py-20 md:py-28 lg:py-52 px-10 md:px-28 lg:px-48 xl:px-72 2xl:px-96"
       id="about"
     >
       <ScrollAnimation animateIn="fade" duration={0.75}>
@@ -13,13 +13,21 @@ function About() {
             About me
           </h2>
         </div>
-        <p className="mb-5">
-          Since beginning my journey as a freelance designer nearly 10 years
-          ago, I've done remote work for agencies, consulted for startups, and
-          collaborated with talented people to create digital products for both
-          business and consumer use. I'm quietly confident, naturally curious,
-          and perpetually working on improving my chops one design problem at a
-          time.
+        <p className="text-md">
+          Since beginning my journey as a software engineer approximately 4
+          years ago, I've done some freelance remote work, worked full-time in
+          small to medium-sized companies, and additionally, have worked on
+          personal projects continuously. Being a self-taught engineer, helped
+          me learn to perpetually work on improving my skill set. Combined with
+          my passion for trying out and creating new things, it is only natural
+          that I have spent most of my time dedicated to coding so far. And It
+          has paid off! Up to this point I have worked with most modern web
+          development tools, languages and, frameworks. Therefore, I am
+          confident in my competencies and can assure creating and maintaining
+          high-quality code bases by following the industry's best practices and
+          using adequate technologies. Among the people I have worked with so
+          far I am known for my efficacy and also for the depth of my technical
+          knowledge.
         </p>
       </ScrollAnimation>
     </section>
