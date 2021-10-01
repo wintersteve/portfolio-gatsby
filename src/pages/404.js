90c7a7c3eab168684b686cf03be8eca02fe7eaff
Steve Winter <steve.winter@diva-e.com>
2021-10-01 21:09:21 +0200

    feat(404): implement page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,22 @@
-import * as React from "react"
-import { Link } from "gatsby"
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import * as React from "react";
+import { Link } from "gatsby";
+import useDarkMode from "../hooks/use-dark-mode";
 
 // markup
 const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+  useDarkMode();
 
-export default NotFoundPage
+  return (
+    <main className="dark:bg-primary-300 dark:text-white flex flex-col items-center justify-center h-screen">
+      <h1 className="mb-4 font-semibold text-7xl">404</h1>
+      <h2 className="dark:bg-primary-100 mb-10 p-3 rounded-lg text-lg">
+        PAGE NOT FOUND
+      </h2>
+      <Link className="" to="/">
+        Go Back
+      </Link>
+    </main>
+  );
+};
+
+export default NotFoundPage;
