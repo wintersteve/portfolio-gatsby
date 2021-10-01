a7ace1ef3dee555ca270fe025a767cbf9090f376
Steve Winter <steve.winter@diva-e.com>
2021-10-01 08:55:24 +0200

    feat(dark-mode): implement dark mode hook

---
+++ src/hooks/use-dark-mode.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+const THEME = "dark";
+const BACKGROUND = ["dark:bg-primary-200", "transition-all"];
+
+const ROOT = window.document.documentElement;
+const BODY = window.document.body;
+
+function useDarkMode() {
+  const darkThemeFromStorage = JSON.parse(
+    window?.localStorage?.darkMode || null
+  );
+
+  const isDarkThemeSet = darkThemeFromStorage !== null;
+  const isDarkThemeActive = isDarkThemeSet && darkThemeFromStorage;
+
+  const isDarkThemePreferred =
+    isDarkThemeActive ||
+    (!isDarkThemeSet &&
+      window?.matchMedia("(prefers-color-scheme: dark)").matches);
+
+  const [isDarkMode, setState] = useState(isDarkThemePreferred);
+
+  const setIsDarkMode = (value) => {
+    setState(value);
+    window?.localStorage?.setItem("darkMode", value);
+  };
+
+  useEffect(() => {
+    console.log(isDarkThemePreferred);
+
+    if (isDarkMode) {
+      ROOT.classList.add(THEME);
+      BODY.classList.add(...BACKGROUND);
+    } else {
+      ROOT.classList.remove(THEME);
+      BODY.classList.remove(...BACKGROUND);
+    }
+  }, [isDarkMode, isDarkThemePreferred]);
+
+  return [isDarkMode, setIsDarkMode];
+}
+
+export default useDarkMode;
