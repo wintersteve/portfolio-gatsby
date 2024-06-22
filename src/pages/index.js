a57ec80295144e65f524d272706ced83c16d1559
Steve Winter <stevewinter@outlook.de>
2024-06-22 12:27:54 +0200

    refactor: move jsonld to Head

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,39 @@ import Footer from "../sections/footer";
 import Projects from "../sections/projects";
 import Seo from "../components/seo";
 import { Helmet } from "react-helmet";
+import competencies from "../data/competencies.json";
+
+export const Head = () => {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    name: "Steve Winter",
+    jobTitle: "Software Engineer",
+    url: "https://wintersteve.com",
+    image: "https://wintersteve.com/images/profile.jpeg",
+    sameAs: [
+      "https://www.linkedin.com/in/steve-winter/",
+      "https://github.com/wintersteve",
+      "https://www.facebook.com/steve.winter.9659",
+    ],
+    email: "mailto:contact@wintersteve.com",
+    description:
+      "Seasoned software engineer with 5+ years of experience in full-stack development, cloud architecture, cybersecurity, and system administration. Proven track record of delivering innovative solutions for SMEs and enterprises.",
+    knowsAbout: [
+      "Web Development",
+      "Frontend Development",
+      "Backend Development",
+      "Software Architecture",
+      "Cloud Computing",
+      "Cybersecurity",
+      ...competencies.flatMap(({ categories }) =>
+        categories.flatMap(({ skills }) => skills.map(({ title }) => title))
+      ),
+    ],
+  };
+
+  return <script type="application/ld+json">{JSON.stringify(jsonLd)}</script>;
+};
 
 const IndexPage = () => {
   return (
